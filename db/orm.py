32983b3fd7edcd965b2137c3b4d38551d041b56c
Mohsen <MohsenAhmadi2003@yahoo.com>
2024-01-28 21:05:24 +0330

    [create]: add login method

--- db/orm.py
+++ db/orm.py
@@ -1,14 +1,54 @@
+# وارد کردن ماژول‌های مورد نیاز
 import mysql.connector
 import configparser
 from db_connection import DatabaseFactory
+import os
 
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
 
+# خواندن اطلاعات از فایل پیکربندی
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(BASEDIR+'\config.ini')
 
-host = config.get('database', 'host')
+# خواندن اطلاعات مورد نیاز از فایل پیکربندی
+host = config.get("database", "host")
 database = config.get('database', 'database')
-user = config.get('database', 'user')
-password = config.get('database', 'password')
+db_username = config.get('database', 'user')
+db_password = config.get('database', 'password')
 
-# connection = DatabaseFactory.create_connection(host, database, user, password)
+
+# تعریف کلاس ORM برای ارتباط با پایگاه داده
+class ORM:
+    @staticmethod
+    def login(username: str, password: str):
+        result = None
+        try:
+            # ایجاد اتصال به پایگاه داده
+            db = DatabaseFactory.create_connection(host, database, db_username, db_password)
+
+            # اگر اتصال برقرار بود
+            if db.is_connected():
+                # ایجاد یک cursor برای اجرای کوئری‌ها
+                cursor = db.cursor()
+
+                # ساخت کوئری برای ایجاد تابع در پایگاه داده
+                query = f"""
+                    SELECT IF(LoginUser('{username}', '{password}'), '1', '0');
+                """
+
+                # اجرای کوئری
+                cursor.execute(query)
+                result = cursor.fetchone()
+
+        except mysql.connector.Error as error:
+            print("خطا در اتصال به پایگاه داده MySQL:", error)
+            return False
+
+        finally:
+            # بستن اتصال
+            if db.is_connected():
+                cursor.close()
+                db.close()
+                print("اتصال MySQL بسته شد.")
+
+        return bool(int(result[0]))  # تبدیل نتیجه به بولین و بازگرداندن آن
