82ad6906776e43236d4c50866daed312074b333f
Mohsen <MohsenAhmadi2003@yahoo.com>
2024-01-28 21:06:51 +0330

    [create]: add user login function

---
+++ sql/functions.sql
@@ -0,0 +1,21 @@
+DELIMITER //
+-- Function for Login 
+CREATE FUNCTION LoginUser(p_username VARCHAR(32), p_password VARCHAR(32))
+RETURNS BOOLEAN
+READS SQL DATA
+BEGIN
+  DECLARE v_user_count INT; 
+
+  SELECT COUNT(*)
+  INTO v_user_count
+  FROM users
+  WHERE username = p_username AND password = p_password;
+
+  IF v_user_count > 0 THEN
+    RETURN TRUE;
+  ELSE
+    RETURN FALSE;
+  END IF;
+END //
+
+DELIMITER ;
