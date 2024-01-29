487ed394140a027b4381761655f2a1deb4d40319
Mohsen <MohsenAhmadi2003@yahoo.com>
2024-01-29 11:41:22 +0330

    [update]: add Process Transaction procedure

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -21,3 +21,69 @@ BEGIN
 END//
 
 DELIMITER ;
+
+
+DELIMITER //
+-- PROCEDURE for Process_Transaction
+CREATE PROCEDURE `Process_Transaction`(
+    IN source_account_number_input VARCHAR(20),
+    IN destination_account_number_input VARCHAR(20),
+    IN amount_input NUMERIC(10, 2),
+    IN description_input VARCHAR(255)
+)
+BEGIN
+    DECLARE _transaction_id INT;
+    DECLARE _secondary_password VARCHAR(8);
+    DECLARE _transaction_date TIMESTAMP;
+    DECLARE rollback_required BOOLEAN DEFAULT FALSE;
+
+    -- Declare continue handler for any exception
+    DECLARE CONTINUE HANDLER FOR SQLEXCEPTION
+        BEGIN
+            SET rollback_required = TRUE;
+        END;
+
+    -- Start the transaction
+    START TRANSACTION;
+
+    SET _transaction_date = NOW();
+    -- Insert transaction record
+    INSERT INTO TRANSACTION (source_account_number, destination_account_number, amount, transaction_date, status,
+                             description)
+    VALUES (source_account_number_input, destination_account_number_input, amount_input, _transaction_date, 'Pending',
+            description_input);
+
+    -- Get the transaction id
+    SELECT id
+    INTO _transaction_id
+    FROM TRANSACTION
+    WHERE source_account_number = source_account_number_input
+      AND destination_account_number = destination_account_number_input
+      AND amount = amount_input
+      AND transaction_date = _transaction_date
+      AND status = 'Pending'
+      AND description = description_input
+    LIMIT 1;
+
+
+    -- Insert secondary password record
+    SET _secondary_password = LPAD(FLOOR(RAND() * POW(10, 8)), 8, '0');
+
+    INSERT INTO SECONDARY_PASSWORDS (bank_account_number, transaction_id, secondary_password, expire_time)
+    VALUES (source_account_number_input, _transaction_id, _secondary_password, TIMESTAMPADD(MINUTE, 2, NOW()));
+
+    -- Check if rollback is required
+    IF rollback_required THEN
+        -- Transaction failed
+        ROLLBACK;
+        SELECT '0' AS Message;
+    ELSE
+        -- Commit the transaction
+        COMMIT;
+        -- Transaction processed successfully
+        SELECT '1' AS Message;
+    END IF;
+
+END//
+
+DELIMITER ;
