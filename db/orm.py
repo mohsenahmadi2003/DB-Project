4a152f2cb75bd6ec8a94196ed4ca8b71427329df
Mohsen <MohsenAhmadi2003@yahoo.com>
2024-01-30 11:32:44 +0330

    [create]: add update password method

--- db/orm.py
+++ db/orm.py
@@ -52,3 +52,38 @@ class ORM:
                 print("اتصال MySQL بسته شد.")
 
         return result
+
+
+    @staticmethod
+    def update_password(old_password: str, new_password: str):
+        result = None
+        try:
+            # ایجاد اتصال به پایگاه داده
+            db = DatabaseFactory.create_connection(host, database, db_username, db_password)
+
+            # اگر اتصال برقرار بود
+            if db.is_connected():
+                # ایجاد یک cursor برای اجرای کوئری‌ها
+                cursor = db.cursor()
+
+                # ساخت کوئری برای ایجاد تابع در پایگاه داده
+                query = f"""
+                        CALL UpdatePassword('{username}', '{password}');
+                """
+
+                # اجرای کوئری
+                cursor.execute(query)
+                result = cursor.fetchone()
+
+        except mysql.connector.Error as error:
+            print("خطا در اتصال به پایگاه داده MySQL:", error)
+            return False
+
+        finally:
+            # بستن اتصال
+            if db.is_connected():
+                cursor.close()
+                db.close()
+                print("اتصال MySQL بسته شد.")
+
+        return result
