3f2eac34c071d6144adaa9fda2361312f072f8cb
Mohsen <MohsenAhmadi2003@yahoo.com>
2024-02-01 22:55:21 +0330

    [update]: change expire time

--- utils/email_message.py
+++ utils/email_message.py
@@ -24,7 +24,7 @@ class EmailNotification:
             <div dir="rtl" style="font-family: Arial, sans-serif; max-width: 600px; margin: 0 auto;">
                 <h2 style="color: #333333; text-align: center;">{password}: رمز دوم</h2>
                 <hr style="border: 0; border-top: 1px solid #eee;">
-                <p style="color: #666666; font-size: 16px; text-align: center;">کاربر گرامی، رمز دوم تا دو دقیقه بعد بیشتر اعتبار ندارد</p>
+                <p style="color: #666666; font-size: 16px; text-align: center;">کاربر گرامی، رمز دوم تا یک دقیقه بعد بیشتر اعتبار ندارد</p>
                 <p style="color: #666666; font-size: 16px; text-align: center;">زمان: <span style="color: #0066cc;">{current_time}</span></p>
             </div>
         """
