36591cac44ed94f8e3ef7b3cb0095559d91eaec0
Mohsen <MohsenAhmadi2003@yahoo.com>
2024-02-02 17:04:58 +0330

    [update]: add get_smallest_unpaid_installment

--- db/orm.py
+++ db/orm.py
@@ -812,6 +812,44 @@ class ORM:
 
         return result
 
+    @staticmethod
+    def get_smallest_unpaid_installment(loan_id_input: int):
+        """
+        ایدی آخرین قسطی که باید پرداخت شود برگردانده می شود
+        ورودی هم ایدی وام هست
+        :param loan_id_input:
+        :return:
+        یک عدد خروجی هست
+        """
+        result = None
+        db = None
+        cursor = None
+        try:
+            # ایجاد اتصال به پایگاه داده
+            db = DatabaseFactory.create_connection(host, database, db_username, db_password)
+
+            # اگر اتصال برقرار بود
+            if db.is_connected():
+                # ایجاد یک cursor برای اجرای کوئری‌ها
+                cursor = db.cursor()
+
+                # ساخت کوئری برای ایجاد تابع در پایگاه داده
+                cursor.execute(F"SELECT GetSmallestUnpaidInstallment({loan_id_input})")
+                result = cursor.fetchone()
+
+        except mysql.connector.Error as error:
+            print("خطا در اتصال به پایگاه داده MySQL:", error)
+            return False
+
+        finally:
+            # بستن اتصال
+            if db.is_connected():
+                cursor.close()
+                db.close()
+                print("اتصال MySQL بسته شد.")
+
+        return result[0]
+
 # print(
 #     ORM.create_transaction(source_account_number=12345678901234567891, destination_account_number=98765432101234567892,
 #                            transfer_amount=600, description="Fake2"))
@@ -820,3 +858,4 @@ class ORM:
 # print(ORM.get_loan_installments('3'))
 # print(ORM.get_loan_payment_status('3'))
 # print(ORM.pay_loan_installment('77773333666633338888','250.00', '3', '25'))
+print(ORM.get_smallest_unpaid_installment('3'))
